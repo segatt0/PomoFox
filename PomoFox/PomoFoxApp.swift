e67f14eccd0945f830f5e38e203378d448111061
segatt0 <anabellesegatto@hotmail.com>
2022-04-11 19:07:43 -0300

    Initial Commit

---
+++ PomoFox/PomoFoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoFoxApp.swift
+//  PomoFox
+//
+//  Created by Moises Vilas Boas on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PomoFoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
